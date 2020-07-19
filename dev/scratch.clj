1e99ccdd1a541e355678a090818582b01c0fc5e0
Francisco Viramontes <kidpollo@gmail.com>
2020-07-19 13:40:51 -0700

    CHAPTER 2: the data model

---
+++ dev/scratch.clj
@@ -0,0 +1,11 @@
+(ns simple-app.specs)
+(s/explain :simple-app/last-name 1)
+(s/explain :simple-app/last-name "foo")
+(s/explain :simple-app/date-of-birth "1/1/1")
+(s/explain :simple-app/date-of-birth (java.util.Date.))
+(s/explain :simple-app/record {})
+(s/explain :simple-app/record {:first-name "foo"
+                               :last-name "bar"
+                               :gender "neutral"
+                               :favorite-color "chartreuse"
+                               :date-of-birth (java.util.Date.)})
---
+++ src/simple_app/spec.clj
@@ -0,0 +1,14 @@
+(ns simple-app.specs
+  (:require [clojure.spec.alpha :as s]))
+
+(s/def :simple-app/last-name string?)
+(s/def :simple-app/first-name string?)
+(s/def :simple-app/gender string?)
+(s/def :simple-app/favorite-color string?)
+(s/def :simple-app/date-of-birth inst?)
+
+(s/def :simple-app/record (s/keys :req-un [:simple-app/first-name
+                                           :simple-app/last-name
+                                           :simple-app/gender
+                                           :simple-app/favorite-color
+                                           :simple-app/date-of-birth]))
